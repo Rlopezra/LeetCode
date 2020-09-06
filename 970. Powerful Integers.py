3f93bfe6730a0fa2c65f6dd6b25037610fcf75a7
Rlopezra <rlopezra@gmail.com>
2020-09-06 01:04:27 -0700

    Create 970. Powerful Integers.py

---
+++ 970. Powerful Integers.py
@@ -0,0 +1,52 @@
+"""
+Given two positive integers x and y, an integer is powerful if it is equal to x^i + y^j for some integers i >= 0 and j >= 0.
+
+Return a list of all powerful integers that have value less than or equal to bound.
+
+You may return the answer in any order.  In your answer, each value should occur at most once.
+
+ 
+
+Example 1:
+
+Input: x = 2, y = 3, bound = 10
+Output: [2,3,4,5,7,9,10]
+Explanation: 
+2 = 2^0 + 3^0
+3 = 2^1 + 3^0
+4 = 2^0 + 3^1
+5 = 2^1 + 3^1
+7 = 2^2 + 3^1
+9 = 2^3 + 3^0
+10 = 2^0 + 3^2
+Example 2:
+
+Input: x = 3, y = 5, bound = 15
+Output: [2,4,6,8,10,14]
+"""
+
+
+
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
+        import math
+        powerful = []
+        
+        if x == 1:
+            max_i = 1
+        else:
+            max_i = int(math.log10(bound)/math.log10(x))+1
+            
+        if y == 1:
+            max_j = 1
+        else:
+            max_j = int(math.log10(bound)/math.log10(y))+1
+        
+        
+        for i in range(max_i):
+            for j in range(max_j):
+                result = (pow(x,i) + pow(y,j))
+                if (result not in powerful) and (result <=bound):
+                    powerful.append(result)
+        
+        return powerful
